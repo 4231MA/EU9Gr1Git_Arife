3a9abd0defbdd010b5614ef526c8dd283799cbc9
Arife <arife4231@hotmail.com>
2022-08-10 20:47:06 +0300

    Second commit from Arife

--- src/Arife.java
+++ src/Arife.java
@@ -3,6 +3,7 @@ public class Arife {
     public static void main(String[] args) {
 
         System.out.println("First change from Arife");
+        System.out.println("Second change from Arife");
 
 
 
